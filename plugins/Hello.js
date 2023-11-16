d7718f5fe8b70cca955b4eaf63296f5f59b8aa5d
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-16 10:36:22 +0530

    Update Hello.js

--- plugins/Hello.js
+++ plugins/Hello.js
@@ -1,3 +1,4 @@
+const pushname = m.pushName || "No Name"
 let handler = async (m) => {
  m.reply(`👋🏻 Hi *${pushname}*, i am  *Maria-Md*📍\nA whatsapp bot created by: Ayush \n\n I don't have time for chit-chat Darling. Use command from *${prefix}help* list if you want me to do anything.
  `.trim()) 
