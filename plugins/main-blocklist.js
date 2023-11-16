6a26415c89fe3d367a5d14ca538fc7b3e5d3648c
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-16 21:37:01 +0530

    Update main-blocklist.js

--- plugins/main-blocklist.js
+++ plugins/main-blocklist.js
@@ -2,11 +2,11 @@
 let handler = async (m, { conn }) => {
 	
 	await conn.fetchBlocklist().then(async data => {
-		let txt = `*here the list of block users*\n\n*Total :* ${data.length}\n\n┌─⊷\n`
+		let txt = `*here the list of block users🚫*\n\n*Total :* ${data.length}\n\n┌─⊷\n`
 		for (let i of data) {
 			txt += `▢ @${i.split("@")[0]}\n`
 		}
-		txt += "└───────────"
+		txt += "└───────────\n\n *if you don't want to block then follow the rules.*"
 		return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 	}).catch(err => {
 		console.log(err);
