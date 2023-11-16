12293b5534df4c884ded4cf3598ea071153908fd
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-16 19:51:21 +0530

    Create support.js

---
+++ plugins/support.js
@@ -0,0 +1,9 @@
+let handler = async (m) => {
+ m.reply(`📍My Developer's Group📍\n\n*🎇 𝐌𝐚𝐫𝐢𝐚 support group:🎇* *https://chat.whatsapp.com/FoS7pSPtfMqBuoireK4aAJ*
+`.trim()) 
+}
+handler.help = ['support]
+handler.tags = ['main']
+handler.command = ['support]
+
+export default handler
