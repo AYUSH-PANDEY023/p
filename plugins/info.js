11de6e3c0f137f65344ac1b2a98a659a62127195
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-16 20:00:37 +0530

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -1,8 +1,8 @@
 let handler = async (m) => {
  m.reply(`🌟『𝕄𝕒𝕣𝕚𝕒-𝕄𝕕 』🌟
 *🌟Description:* A WhatsApp Bot With Rich  features based on Maria
-*🚦Uptime:* ${runtime(process.uptime())}
-*🕸Version:* 1.2.0
+*🚦Uptime:* %uptime
+*🕸Version:* 2.0
 *👤Owner:*  ${global.Owner}
 *🎐supportgc:* ${global.support}\n
 *Powered by Ayush*
